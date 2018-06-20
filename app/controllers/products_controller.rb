dc967335266c61aad5e94cf6aa3dc216f2cb7a25
Jose Antonio H <jahonora@uc.cl>
2018-06-20 00:58:24 -0400

    uploading solution

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,22 @@
+class ProductsController < ApplicationController
+  def new
+  end
+
+  def create
+  	@product = Product.new(product_params)
+  	@product.save
+  	redirect_to category_path(@product.category)
+  end
+
+  def destroy
+  	@product = Product.find(params[:id])
+  	@product.destroy
+  	redirect_to category_path(@product.category)
+  end
+
+  private
+
+  def product_params
+  	params.require(:product).permit(:name, :price, :category_id)
+  end
+end
